0036a73243f583d1807aec1617297fecb2350c8f
voitekhovich <a.voitekhovich@gmail.com>
2023-09-25 22:32:12 +0300

    feat: Start project. Add header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <a className="header__logo" href="#">AV<span>code</span></a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
