222cf03658f36b96fc0ca2a95f462d6bead81dad
voitekhovich <a.voitekhovich@gmail.com>
2023-10-17 23:08:11 +0300

    feat: add animation for pages

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,5 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import routeConfig from "@routes/routeConfig";
+
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 import Header from "@components/Header";
 import Footer from "@components/Footer";
@@ -7,23 +10,43 @@ import Footer from "@components/Footer";
 import "./App.css";
 
 function App() {
+  const location = useLocation();
+  const { nodeRef } =
+    routeConfig.find((route) => route.path === location.pathname) ?? {};
+
   return (
-    <BrowserRouter>
-      <div className="app">
-        <Header />
-        <Routes>
-          {routeConfig.map((route) => (
-            <Route
-              key={route.path}
-              path={route.path}
-              end={route.end}
-              element={route.element}
-            />
-          ))}
-        </Routes>
-        <Footer />
+    <div className="app">
+      <Header />
+
+      <div className="container">
+        <TransitionGroup>
+          <CSSTransition
+            key={location.pathname}
+            nodeRef={nodeRef}
+            timeout={500}
+            classNames="page"
+            unmountOnExit
+          >
+            {(state) => (
+              <div ref={nodeRef} className="page">
+                <Routes location={location}>
+                  {routeConfig.map((route) => (
+                    <Route
+                      key={route.path}
+                      path={route.path}
+                      end={route.end}
+                      element={route.element}
+                    />
+                  ))}
+                </Routes>
+              </div>
+            )}
+          </CSSTransition>
+        </TransitionGroup>
       </div>
-    </BrowserRouter>
+
+      <Footer />
+    </div>
   );
 }
 
